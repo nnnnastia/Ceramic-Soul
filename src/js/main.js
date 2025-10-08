eb21066d93d5e661a9ee579b739adfe51bf7e165
Style <v.melenchuk03@gmail.com>
2025-10-08 20:32:34 +0300

    Added works section

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,41 @@
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 import "/src/sass/style.scss";
+
+try {
+    // init Swiper:
+    const swiper = new Swiper('.works__slider', {
+
+        slidesPerView: 1,
+        loop: true,
+        modules: [Navigation, Pagination],
+
+        navigation: {
+            nextEl: '.icon-right-open',
+            prevEl: '.icon-left-open',
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+
+        breakpoints: {
+            // when window width is >= 1200px
+            1200: {
+                slidesPerView: 3,
+                spaceBetween: 5
+            },
+            // when window width is >= 1920px
+            1920: {
+                slidesPerView: 3,
+                spaceBetween: 35
+            }
+        }
+    });
+} catch (e) {
+
+}
